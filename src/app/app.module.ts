b4fd22cd68ffbbb3fca9127808df336a751d9195
javiergg13 <javiergg13@educastur.es>
2022-04-21 09:01:36 +0200

    fdas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { ListadoComponent } from './views/listado/listado.component';
-import { EntradaComponent } from './views/listado/entrada/entrada.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
 import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
@@ -16,8 +14,6 @@ import { LoginComponent } from './views/login/login.component';
   declarations: [
     AppComponent,
     MenuComponent,
-    ListadoComponent,
-    EntradaComponent,
     PaginaNoEncontradaComponent,
     AcercaDeNosotrosComponent,
     LoginComponent
