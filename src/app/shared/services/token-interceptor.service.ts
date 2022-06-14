07b93c5dda0c093e177683efd463032bcb1a7df3
CORP\javiguer <guerrerogallojavier@gmail.com>
2022-06-14 11:01:06 +0200

    remove imports unsed

--- src/app/shared/services/token-interceptor.service.ts
+++ src/app/shared/services/token-interceptor.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core'
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { HttpInterceptor } from '@angular/common/http'
 import { LoginService } from './login.service'
 
 @Injectable({
